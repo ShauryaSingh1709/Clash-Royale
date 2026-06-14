caa0fd3d10553326a63958f6586836a7fef45ee7
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-14 22:49:21 +0530

    fixes

--- backend/app.py
+++ backend/app.py
@@ -155,8 +155,9 @@ def create_app() -> Flask:
     return app
 
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(
         host=Config.HOST,
         port=Config.PORT,
